21afe083fd01f3d1ff83baa155c49a3a6d31287a
Kameleon008 <krzysztofkochanowski96@gmail.com>
2020-12-06 14:21:29 +0100

    added new Class Game

---
+++ Cwiczenie 1/Game.h
@@ -0,0 +1,18 @@
+#include <SFML/Graphics.hpp>
+
+#pragma once
+class Game
+{
+public:
+	Game();
+	void run();
+private:
+	void processEvents();
+	void update();
+	void render();
+
+private:
+	sf::RenderWindow mWindow;
+	sf::CircleShape mPlayer;
+
+};
